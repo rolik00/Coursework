3df0d5c26fb857cceb649539d2ed13925157f864
rolik00 <lelya_rodina_01@mail.ru>
2024-12-21 12:02:07 +0300

    Создание проекта

---
+++ src/main/java/ru/coursework/MinorsHSEFeedback/MinorsHseFeedbackApplication.java
@@ -0,0 +1,13 @@
+package ru.coursework.MinorsHSEFeedback;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MinorsHseFeedbackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MinorsHseFeedbackApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ru/coursework/MinorsHSEFeedback/MinorsHseFeedbackApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.coursework.MinorsHSEFeedback;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MinorsHseFeedbackApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
