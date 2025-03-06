cbb9e224f07aeb2eb001999e994e49835af1c16d
rolik00 <lelya_rodina_01@mail.ru>
2025-03-06 14:43:12 +0300

    добавление корса

--- src/main/java/ru/coursework/MinorsHSEFeedback/config/WebConfig.java
+++ src/main/java/ru/coursework/MinorsHSEFeedback/config/WebConfig.java
@@ -8,6 +8,6 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
-        registry.addMapping("/**").allowedOrigins("http://localhost:3000");
+        registry.addMapping("/**").allowedOrigins("http://localhost:3000","http://localhost:3001","http://localhost:3002");
     }
 }
