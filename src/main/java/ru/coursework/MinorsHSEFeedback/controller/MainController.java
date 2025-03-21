974881558b4293af54c96efc90c48d0fecb2a1e4
rolik00 <lelya_rodina_01@mail.ru>
2025-03-21 15:38:42 +0300

    из GET-запросов в POST

--- src/main/java/ru/coursework/MinorsHSEFeedback/controller/MainController.java
+++ src/main/java/ru/coursework/MinorsHSEFeedback/controller/MainController.java
@@ -4,6 +4,7 @@ import io.swagger.v3.oas.annotations.Operation;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -46,26 +47,26 @@ public class MainController {
     }
 
     @Operation(summary = "Получить все майноры заданным категориям")
-    @GetMapping("/categories")
+    @PostMapping("/categories")
     public List<UiMinor> getMinorsByCategory(@RequestBody Set<Long> categoryIds) {
         return minorService.findAllMinorsByCategoryIds(categoryIds);
     }
 
     @Operation(summary = "Отсортировать все полученные майноры по одному из рейтингов")
-    @GetMapping("/minors_sort")
+    @PostMapping("/minors_sort")
     public List<UiMinor> getSortMinors(@RequestBody String comparator) {
         return minorService.findSortedAllMinors(Sort.valueOf(comparator).getComparator());
     }
 
     @Operation(summary = "Отсортировать все полученные по заданным категориям майноры")
-    @GetMapping("/categories_sort")
+    @PostMapping("/categories_sort")
     public List<UiMinor> getSortMinorsByCategory(@RequestBody SortAllMinorsByCategoriesRequest request) {
         log.info("getSortMinorsByCategory {}", request);
         return minorService.findSortedAllMinorsByCategoryIds(request.getCategoryIds(), Sort.valueOf(request.getComparator()).getComparator());
     }
 
     @Operation(summary = "Получить майноры по id для сравнительной таблицы")
-    @GetMapping("/comparison_table")
+    @PostMapping("/comparison_table")
     public List<UiMinor> getMinorsForComparisonTable(@RequestBody MinorsForCTRequest request) {
         return minorService.getMinorsForComparisonTable(request.getIds());
     }
