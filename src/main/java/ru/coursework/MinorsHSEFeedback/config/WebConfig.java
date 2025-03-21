a9f6b4b71213be2619c853071772e6d522aae340
rolik00 <lelya_rodina_01@mail.ru>
2025-03-21 15:50:04 +0300

    разрешение методов для корса

--- src/main/java/ru/coursework/MinorsHSEFeedback/config/WebConfig.java
+++ src/main/java/ru/coursework/MinorsHSEFeedback/config/WebConfig.java
@@ -1,13 +1,19 @@
 package ru.coursework.MinorsHSEFeedback.config;
 
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+import java.util.Arrays;
 
 @Configuration
 public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
-        registry.addMapping("/**").allowedOrigins("http://localhost:3000","http://localhost:3001","http://localhost:3002");
+        var allHttpMethods = Arrays.stream(HttpMethod.values()).map(HttpMethod::name).toList();
+        registry.addMapping("/**")
+                .allowedOrigins("http://localhost:3000","http://localhost:3001","http://localhost:3002")
+                .allowedMethods("GET", "POST", "PUT", "PATCH", "TRACE", "DELETE", "OPTIONS");
     }
 }
