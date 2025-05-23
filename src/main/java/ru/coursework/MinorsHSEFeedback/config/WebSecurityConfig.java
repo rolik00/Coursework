760d594075f30db079ba5166173bd1d415cff4b0
rolik00 <lelya_rodina_01@mail.ru>
2025-05-23 22:01:45 +0300

    Подключение финальной ссылки фронта

--- src/main/java/ru/coursework/MinorsHSEFeedback/config/WebSecurityConfig.java
+++ src/main/java/ru/coursework/MinorsHSEFeedback/config/WebSecurityConfig.java
@@ -57,7 +57,7 @@ public class WebSecurityConfig {
 			public CorsConfiguration getCorsConfiguration(HttpServletRequest request) {
 				CorsConfiguration corsConfiguration = new CorsConfiguration();
 				corsConfiguration.setAllowCredentials(true);
-				corsConfiguration.setAllowedOrigins(Arrays.asList("http://localhost:3000", "http://localhost:3001", "http://localhost:3002"));
+				corsConfiguration.setAllowedOrigins(Arrays.asList("http://localhost:3000", "https://mak48.github.io/reactCoursework/"));
 				corsConfiguration.setAllowedMethods(Collections.singletonList("*"));
 				corsConfiguration.setAllowedHeaders(Collections.singletonList("*"));
 				corsConfiguration.setMaxAge(Duration.ofMinutes(5L));
